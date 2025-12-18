54f2b24379d2cc9a29c2f3b45244df1288c6b190
Marc'Antonio Lopez <marcantoniolopez0@gmail.com>
2025-12-18 21:07:34 +0100

    feat: enable configurable LLM model, refactor prompt templates, and adjust generation parameters.

--- tests/verify_generation.py
+++ tests/verify_generation.py
@@ -6,6 +6,10 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from src.generation import create_generation_components
 
+# CONFIGURATION
+# MODEL_ID = "meta-llama/Meta-Llama-3.1-8B-Instruct" # Standard
+MODEL_ID = "meta-llama/Llama-3.2-3B-Instruct" # Light for local testing
+
 if __name__ == "__main__":
     progress_bar = tqdm(
         total=100,
@@ -14,17 +18,17 @@ if __name__ == "__main__":
     )
 
     print("--- GENERATION TESTS STARTED ---")
-    components = create_generation_components()
+    components = create_generation_components(model_id=MODEL_ID)
     progress_bar.update(10)
 
     print("--- TEST GENERATOR (POSITIVE) ---")
     res = components.generator.invoke(
         {
             "context": "Tim Cook is the CEO of Apple.",
-            "question": "Who runs Apple?"
+            "question": "Who is the CEO of Apple?"
         }
     )
-    print(f"Output: {res}\nExpected: Somethink like 'Tim Cook'")
+    print(f"Output: {res}\nExpected: Somethink like 'Tim Cook'\n\n")
     progress_bar.update(50)
 
     print("--- TEST GENERATOR (NEGATIVE) ---")
@@ -34,19 +38,20 @@ if __name__ == "__main__":
             "question": "Who is the CEO of Apple?"
         }
     )
-    print(f"Output: {res}\nExpected: 'I_DONT_KNOW'")
+    print(f"Output: {res}\nExpected: 'I_DONT_KNOW'\n\n")
     progress_bar.update(90)
 
     print("--- TEST REWRITER---")
     res = components.query_rewriter.invoke(
         {
             "messages": [
-                ("human", "Who is the creator of Python?")
+                ("human", "Who is the creator of Python?"),
+                ('assistant', 'Guido van Rossum')
             ],
             "question": "When was he born?"
         }
     )
-    print(f"Output: {res}\nExpected: 'When was Guido van Rossum born?'")
+    print(f"Output: {res}\nExpected: 'When was Guido van Rossum born?'\n\n")
     progress_bar.update(100)
 
     print("--- GENERATION TESTS COMPLETED ---")
